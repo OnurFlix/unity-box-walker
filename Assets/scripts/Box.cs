89fd5a4451990e12bc787d5a43b41f61da1ad885
OnurFlix <onurzamanla@protonmail.com>
2025-07-19 23:08:28 +0300

    Updated Box.cs: Switching from Flappy Bird to CS-style WASD movement syste

--- Assets/scripts/Box.cs
+++ Assets/scripts/Box.cs
@@ -2,28 +2,38 @@ using UnityEngine;
  
 public class Box : MonoBehaviour
 {   
-    public bool HareketEtmek; 
     public float speed = 5f;
-    // Start is called once before the first frame update
+    
     void Start()
     {
         print("hello world");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.forward * speed * Time.deltaTime;
-      
-      
-        if (Input.GetMouseButton(0)) 
-        {  
-            transform.position += Vector3.up * speed * Time.deltaTime; 
+        if (Input.GetKey(KeyCode.W)) // W tuşu basılıysa ileri (yürüme)
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S)) // S tuşu basılıysa geri
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A)) // A tuşu basılıysa sola
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D)) // D tuşu basılıysa sağa
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
         } 
-        else 
+        if (Input.GetKey(KeyCode.LeftShift)) // Shift tuşu basılıysa aşağı 
+        {
+           transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.Space)) // Space tuşu basılıysa yukarı 
         {  
-            transform.position += Vector3.down * speed * Time.deltaTime; 
-        } 
-} 
-
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+    } 
 }
